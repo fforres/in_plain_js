5d2637c3c33d52d05d2fc6c44ce0393eba9e46ff
Felipe Torres <Felipe Torres>
2017-01-19 22:34:04 -0300

    Adding service workers

--- netflix/index.js
+++ netflix/index.js
@@ -1,9 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     if(!!ENV) {
-      navigator.serviceWorker.register('/js/service_workers/general.js', {
-        scope: window.location.href
-      }).then(function(registration) {
+      navigator.serviceWorker.register('./general.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }).catch(function(err) {
--- netflix/js/config.js
+++ netflix/js/config.js
@@ -2,6 +2,7 @@ window.youtubeIds = [
   'UC7tUsO3S7424TMcgSCUOCow', //Noders
   'UCO1cgjhGzsSYb1rsB4bFe4Q', //Fun fun function
   'UCzoVCacndDCfGDf41P-z0iA', //JSConf
+  'UCoebwHSTvwalADTJhps0emA', //JSConf
 ]
 
 ENV = (()=>(this.location.host.lastIndexOf('localhost') !== -1))()
